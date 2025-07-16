94b65f765bf430fa7c5502bf115c8b1b48739c37
andreusxcarvalho <andreusrobycarvalho@gmail.com>
2025-07-16 11:34:02 +0800

    update

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -2,7 +2,7 @@ export const projectsData = [
     {
         id: 1,
         name: 'CLI Fitness Tracker',
-        description: "",
+        description: "Built a command-line fitness tracker in Java with features like daily logging, progress tracking, and file persistence using OOP principles and JUnit testing.",
         skills: ['Java', 'GitHub', 'OOP', 'SDLC', 'Software Testing', 'JSON', 'Technical Documentation'],
         role: 'Backend Developer',
         code: 'https://github.com/andreusxcarvalho/tp',
@@ -11,7 +11,7 @@ export const projectsData = [
     {
         id: 2,
         name: 'Veristrike (Digital Design FPGA Game)',
-        description: '',
+        description: 'Designed and implemented a Verilog-based interactive combat game on FPGA using the Basys 3 board, featuring sprite movement, collision detection, and OLED display rendering',
         skills: ['Verilog', 'Digital Circuit Design', "Embedded Systems Programming", "Modular Hardware Design", "Finite State Machine"],
         role: 'FPGA Developer',
         code: 'https://github.com/andreusxcarvalho/Digital-Design-FPGA-Project',
@@ -20,7 +20,7 @@ export const projectsData = [
     {
         id: 3,  
         name: 'Portfolio Website',
-        description: '',
+        description: 'Built and deployed a responsive personal portfolio using Next.js, showcasing projects & experience. Integrated contact forms with email and Telegram notifications.',
         skills: ['NextJS', 'Tailwind', 'UI/UX Design', 'Web Development', 'RESTful API'],
         code: 'https://github.com/andreusxcarvalho/developer-portfolio',
         role: 'Frontend Developer',
@@ -29,7 +29,7 @@ export const projectsData = [
     {
         id: 4,
         name: 'mBot Maze-Solving Robot Project',
-        description: "",
+        description: "Built an autonomous mBot maze solver using custom IR sensors and C++ firmware, featuring real-time wall detection, PID-based navigation, and dynamic pathfinding.",
         skills: ['C++', 'Circuit Design', 'Arduino', 'Hardware-Software Integration'],
         code: 'https://github.com/ShengBin-101/CG1111A-Final-Project',
         demo: '',
