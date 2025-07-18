dfa9426f469ff86ad3682bc22b9920bdc27b202b
andreusxcarvalho <andreusrobycarvalho@gmail.com>
2025-07-18 16:52:38 +0800

    update

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,17 @@
-"use client"
+'use client';
 
-import Lottie from "lottie-react";
+import dynamic from 'next/dynamic';
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// Disable SSR for this component
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
+const AnimationLottie = ({ animationPath, className = '' }) => {
   return (
-    <Lottie {...defaultOptions} />
+    <Lottie
+      animationData={animationPath}
+      loop={true}
+      className={className}
+    />
   );
 };
 
